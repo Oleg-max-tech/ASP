44a9cf2f51a31a4d114fa773fb7c76326dc418a6
oleg <User@DESKTOP-JLMGOG7>
2024-12-18 14:25:51 +0200

    Add project files.

---
+++ ASP1/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using ASP1.Models;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ASP1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult ProductsList()
+        {
+            // Створення тестового продукту
+            Product product = new Product
+            {
+                Name = "Смартфон",
+                Price = 15000.50m,
+                Description = "Смартфон з великим екраном та потужним акумулятором",
+                ManufactureDate = new DateTime(2024, 10, 15)
+            };
+
+            // Передача даних у View
+            ViewBag.Description = product.Description;
+            ViewData["ManufactureDate"] = product.ManufactureDate.ToString("yyyy-MM-dd");
+
+            return View(product);
+        }
+    }
+}
---
+++ ASP1/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP1.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public DateTime ManufactureDate { get; set; }
+    }
+}
